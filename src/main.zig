2dcb10d62f2939a0a39c24328b5f3de414475bbe
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-04-15 18:39:04 +0200

    Added sdl window

--- src/main.zig
+++ src/main.zig
@@ -3,23 +3,27 @@ const sdl = @import("sdl2");
 const vk = @import("vulkan");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    try sdl.init(.{
+        .video = true,
+        .events = true,
+    });
+    defer sdl.quit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var window = try sdl.createWindow("Test window", .{.centered = {}}, .{.centered = {}}, 800, 600, .{.vis = .shown});
+    defer window.destroy();
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    mainLoop: while(true){
+        while(sdl.pollEvent()) |ev|{
+            switch(ev){
+                .quit => break :mainLoop,
+                .key_down => |key| {
+                    switch(key.scancode){
+                        .escape => break :mainLoop,
+                        else => std.log.info("key pressed: {}\n", .{key.scancode})
+                    }
+                },
+                else => {}
+            }
+        }
+    }
 }
