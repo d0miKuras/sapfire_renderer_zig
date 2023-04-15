69309d338571a1d6f4a3147bdc9512bf2a449352
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-04-15 19:01:25 +0200

    Test sdl window

--- src/main.zig
+++ src/main.zig
@@ -2,27 +2,31 @@ const std = @import("std");
 const sdl = @import("sdl2");
 const vk = @import("vulkan");
 
+const BaseDispatch = vk.BaseWrapper(.{
+    .createInstance = true,
+});
+
+const InstanceDispatch = vk.InstanceWrapper(.{ .destroy_instance = true });
+
 pub fn main() !void {
     try sdl.init(.{
         .video = true,
         .events = true,
     });
     defer sdl.quit();
-
-    var window = try sdl.createWindow("Test window", .{.centered = {}}, .{.centered = {}}, 800, 600, .{.vis = .shown});
+    var window = try sdl.createWindow("Test window", .{ .centered = {} }, .{ .centered = {} }, 800, 600, .{ .vis = .shown, .context = .vulkan });
     defer window.destroy();
-
-    mainLoop: while(true){
-        while(sdl.pollEvent()) |ev|{
-            switch(ev){
+    mainLoop: while (true) {
+        while (sdl.pollEvent()) |ev| {
+            switch (ev) {
                 .quit => break :mainLoop,
                 .key_down => |key| {
-                    switch(key.scancode){
+                    switch (key.scancode) {
                         .escape => break :mainLoop,
-                        else => std.log.info("key pressed: {}\n", .{key.scancode})
+                        else => std.log.info("key pressed: {}\n", .{key.scancode}),
                     }
                 },
-                else => {}
+                else => {},
             }
         }
     }
